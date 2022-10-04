f3e278cc66012f2e6963fb054450a12b3281b867
Israel Sanchez <Israel.pro1994@gmail.com>
2022-10-04 16:33:46 -0500

    reviewing small basics

---
+++ index.ts
@@ -0,0 +1,3 @@
+console.log("hello my guys");
+
+let age: number = 20;
---
+++ typescriptNotes.ts
@@ -0,0 +1,15 @@
+// TypeScript is a programming language created to address the shortcomings of JavaScript
+// Benefits: 
+//    **Static Typing
+//    **Code Completion
+//    **Refactoring
+//    **Shorthand Notations
+
+// Statically-Typed Languages: C++, C#, Java
+// Dynamically-Typed Languages: JavaScript, Python, Ruby
+
+// BASICALLY: TypeScript is just Javascript WITH Type Checking
+
+// Medium to Large projects: TypeScript - Simple Projects: JavaScript
+
+// TRANSPILATION is when TypeScript translates into JavaScript
